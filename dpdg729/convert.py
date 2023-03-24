7f466d91bdd2f36c93a5da61129f9354b0d8beab
Ranjith <ranjith19@gmail.com>
2023-03-24 11:33:26 +0530

    change to python package

---
+++ dpdg729/convert.py
@@ -0,0 +1,27 @@
+import os
+import subprocess
+import logging
+
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger('g729converter')
+cwd = os.path.dirname(os.path.realpath(__file__))
+
+executable = os.path.join(cwd, 'tst/encoderTest')
+
+class ConversionError(Exception):
+    pass
+
+def convert(file_path):
+    expected = file_path.replace('.wav', '.G749')
+    output = subprocess.run([executable, file_path])
+    if os.path.exists(expected):
+        log.error("Unable to convert file: %s due to %s" % (file_path, output))
+        raise ConversionError("Unable to convert file: %s due to %s" % (file_path, output))
+    return expected
+
+
+
+
+if __name__ == "__main__":
+    f = convert('/home/ubuntu/test.wav')
+    log.info(f"File is at {f}")
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='dpdg729',
+      version='0.1',
+      description='Convert wav to g729',
+      url='https://github.com/Vipatra/g729converter',
+      author='Ranjith BR',
+      author_email='ranjith@dpdzero.com',
+      license='Closed source',
+      packages=['dpdg729'],
+      zip_safe=False)
