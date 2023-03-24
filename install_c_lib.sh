24823d70bc678715e627bac0019301732336d800
Ranjith <ranjith19@gmail.com>
2023-03-24 19:56:37 +0530

    small change

--- install_c_lib.sh
+++ install_c_lib.sh
@@ -10,7 +10,7 @@ cd dpdg729
 install(){
     install_prefix=$1
     echo "Installing to ${install_prefix}"
-    cmake . -DCMAKE_INSTALL_PREFIX=${install_prefix} -DCMAKE_PREFIX_PATH=${install_prefix} -DENABLE_TESTS=YES
+    cmake . -DCMAKE_INSTALL_PREFIX=${install_prefix} -DCMAKE_PREFIX_PATH=${install_prefix} -DENABLE_TESTS=YES -DENABLE_SHARED=NO
     make
     make install
     mv test/encoderTest $2/g749Encoder
