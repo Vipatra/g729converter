5b64d966f72f534c6b73166c0287a439c6f2ffa6
Ranjith <ranjith19@gmail.com>
2023-03-24 20:05:13 +0530

    wip

--- dpdg729/src/typedef.h
+++ dpdg729/src/typedef.h
@@ -26,8 +26,8 @@
 #endif
 
 #include "codecParameters.h"
-#include "bcg729/encoder.h"
-#include "bcg729/decoder.h"
+#include "encoder.h"
+#include "decoder.h"
 
 typedef int16_t word16_t;
 typedef uint16_t uword16_t;
