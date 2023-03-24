381f7f6dab6cb06020057e801f28fa2f0cc5c5e2
Ranjith <ranjith19@gmail.com>
2023-03-24 18:49:20 +0530

    wip

--- dpdg729/convert.py
+++ dpdg729/convert.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 import logging
+import sys
 
 logging.basicConfig(level=logging.INFO)
 log = logging.getLogger('g729converter')
@@ -10,9 +11,10 @@ cwd = os.path.dirname(os.path.realpath(__file__))
 class ConversionError(Exception):
     pass
 
+
 def convert_wav_to_g749(file_path):
     expected = file_path.replace('.wav', '.G749')
-    output = subprocess.run(["g749Encoder", file_path])
+    output = subprocess.run(["g749Encoder", file_path], env={'LD_LIBRARY_PATH': os.path.join(sys.prefix, 'lib')}, capture_output=True)
     if os.path.exists(expected):
         log.error("Unable to convert file: %s due to %s" % (file_path, output))
         raise ConversionError("Unable to convert file: %s due to %s" % (file_path, output))
