97463f30d98f88090eafd873cfa6edfcc782deb4
Ranjith <ranjith19@gmail.com>
2023-03-24 18:05:53 +0530

    wip

--- install_c_lib.sh
+++ install_c_lib.sh
@@ -9,6 +9,7 @@ cd dpdg729
 
 install(){
     install_prefix=$1
+    echo "Installing to ${install_prefix}"
     cmake . -DCMAKE_INSTALL_PREFIX=${install_prefix} -DCMAKE_PREFIX_PATH=${install_prefix} -DENABLE_TESTS=YES
     make
     make install
