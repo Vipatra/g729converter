78ae731a8d3138433e65b738bf474a288094926e
Ranjith <ranjith19@gmail.com>
2023-03-24 18:42:14 +0530

    convert

--- dpdg729/convert.py
+++ dpdg729/convert.py
@@ -6,14 +6,13 @@ logging.basicConfig(level=logging.INFO)
 log = logging.getLogger('g729converter')
 cwd = os.path.dirname(os.path.realpath(__file__))
 
-executable = os.path.join(cwd, 'tst/encoderTest')
 
 class ConversionError(Exception):
     pass
 
 def convert_wav_to_g749(file_path):
     expected = file_path.replace('.wav', '.G749')
-    output = subprocess.run([executable, file_path])
+    output = subprocess.run(["g749Encoder", file_path])
     if os.path.exists(expected):
         log.error("Unable to convert file: %s due to %s" % (file_path, output))
         raise ConversionError("Unable to convert file: %s due to %s" % (file_path, output))
